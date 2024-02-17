0d1fd5b6114921d4dec4682dc81919043f507d57
Thijs Miedema <mail@tmiedema.com>
2024-02-17 10:55:55 +0100

    feat: add basic examples

---
+++ examples/hello_world.py
@@ -0,0 +1,69 @@
+from pocketbase import PocketBase, PocketbaseError
+
+
+CONNECTION_URL = "http://localhost:8123"
+ADMIN_EMAIL = "test@example.com"
+ADMIN_PASSWORD = "test"
+
+
+async def hello_world():
+    # Instantiate the PocketBase connector
+    pb = PocketBase("http://localhost:8123")
+
+    # Authenticate as an admin
+    await pb.admin.auth.with_password(email=ADMIN_EMAIL, password=ADMIN_PASSWORD)
+
+    # Create a collection to store records in
+    # It is a base collection (not "view" or "auth") with one column "content"
+    # and it will have the regular "id", "created" and "updated" columns.
+    try:
+        await pb.collections.create(
+            {
+                "name": "hello_world",
+                "type": "base",
+                "schema": [
+                    {
+                        "name": "content",
+                        "type": "text",
+                        "required": True,
+                    },
+                ],
+            }
+        )
+    except PocketbaseError:
+        # You probably ran this example before, and the collection already exists!
+        # No problem, we'll continue as normal :)
+        pass
+    
+    # Get the collection instance we can work with
+    collection = pb.collection("hello_world")
+
+    # Add a new record.
+    await collection.create(params={"content": "Hello, World!"})
+
+    # All the different ways to get records
+    first = await collection.get_first()
+    list_records = await collection.get_list(page=1, per_page=10)
+    all_records = await collection.get_full_list()
+    one = await collection.get_one(record_id=first['id'])
+
+    print(one)
+
+    # Update a record
+    updated = await collection.update(
+        record_id=one['id'],
+        params={'contents': "Good to see you again!"}
+    )
+
+    print(updated)
+
+    # Delete a record
+    await collection.delete(
+        record_id=one['id']
+    )
+
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(hello_world())
---
+++ examples/realtime_updates.py
@@ -0,0 +1,44 @@
+from pocketbase import PocketBase
+from pocketbase.services.realtime import RealtimeEvent
+from datetime import datetime
+import asyncio
+
+
+CONNECTION_URL = "http://localhost:8123"
+ADMIN_EMAIL = "test@example.com"
+ADMIN_PASSWORD = "test"
+
+
+async def callback(event: RealtimeEvent) -> None:
+    # This will get called for every event
+    # Lets print what is going on
+    at = datetime.now().isoformat()
+    print(f"[{at}] {event['action'].upper()}: {event['record']}")
+
+
+async def realtime_updates():
+    # Instantiate the PocketBase connector
+    pb = PocketBase("http://localhost:8123")
+
+    # Authenticate as an admin
+    await pb.admin.auth.with_password(email=ADMIN_EMAIL, password=ADMIN_PASSWORD)
+
+    # Subscribe to all events
+    unsubscribe = await pb.realtime.subscribe(
+        topic="*",
+        callback=callback
+    )
+
+    # Wait for ctrl-c
+    try:
+        while True:
+            await asyncio.sleep(10)
+    except KeyboardInterrupt:
+        pass
+
+    # Unsubscribe using the function returned by the subscribe action
+    await unsubscribe()
+
+
+if __name__ == "__main__":
+    asyncio.run(realtime_updates())
