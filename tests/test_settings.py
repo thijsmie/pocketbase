d7293e40178f7232b70227febf398e25e2e88a2c
Jannis Schönleber <joennlae@gmail.com>
2025-05-30 22:49:15 +0200

    chore: add settings property

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -7,14 +7,14 @@ from pocketbase import PocketBase, PocketBaseError
 
 async def test_write_setting(superuser_client: PocketBase):
     app_name = uuid4().hex
-    await superuser_client._settings.update(
+    await superuser_client.settings.update(
         body={
             "meta": {
                 "appName": app_name,
             },
         }
     )
-    settings = await superuser_client._settings.get_all()
+    settings = await superuser_client.settings.get_all()
     assert settings["meta"]["appName"] == app_name
 
 
@@ -22,5 +22,5 @@ async def test_s3(superuser_client: PocketBase):
     # The context manager pytest.raises works with async code
     with pytest.raises(PocketBaseError) as exc:
         # The new test_s3 method can take 'options'
-        await superuser_client._settings.test_s3()
+        await superuser_client.settings.test_s3()
     assert exc.value.status == 400
