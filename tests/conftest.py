fa1a4923e8ff4e50b50ccc39f7a74e384c673af9
Jannis Schönleber <joennlae@gmail.com>
2025-06-03 16:50:14 +0200

    test(email): add simple smtp server to check if email is sent out

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,9 +7,38 @@ from subprocess import DEVNULL, Popen
 from time import sleep
 
 import pytest
+from aiosmtpd.controller import Controller
 
 from pocketbase import PocketBase
 from tests.prep import ensure_pocketbase_executable
+
+SMTP_PORT = 8025
+SMTP_HOST = "localhost"
+
+
+class MessageHandler:
+    def __init__(self):
+        self.messages = []
+
+    async def handle_DATA(self, _server, _session, envelope):
+        self.messages.append(envelope)
+        return "250 Message accepted for delivery"
+
+
+@pytest.fixture(scope="function")
+def smtp_server():
+    handler = MessageHandler()
+    controller = Controller(handler, hostname=SMTP_HOST, port=SMTP_PORT)
+    controller.start()
+
+    try:
+        yield {
+            "host": SMTP_HOST,
+            "port": SMTP_PORT,
+            "handler": handler,
+        }
+    finally:
+        controller.stop()
 
 
 def find_free_port() -> int:
@@ -39,7 +68,7 @@ def process(superuser: tuple[str, str], port: int, executable: Path, tmpdir_fact
     directory = tmpdir_factory.mktemp("data")
     # Adding a --dev in the command below can be helpful when debugging tests
     p = Popen(
-        args=["_", "serve", f"--dir={directory}", f"--http=127.0.0.1:{port}"],
+        args=["_", "serve", f"--dir={directory}", f"--http=127.0.0.1:{port}", "--dev"],
         executable=executable,
     )
     sleep(0.3)
